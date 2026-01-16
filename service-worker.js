2c79387b91c092218e48f82fee5833f6b45a79f8
EliXTech <elias.gallathe@web.de>
2026-01-16 16:28:29 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => {
+  console.log("Service Worker installiert");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service Worker aktiviert");
+});
